dbc04b7e5c53f0d89993d8e29fe0996c5e9606b9
ZETES\adrian.vergara <adrian.vergara@sf.zetes.com>
2018-08-22 10:11:54 +0200

    Functional Barcode Scanner

---
+++ BarcodeReader/BarcodeReader/Views/BarcodeReaderView.xaml.cs
@@ -0,0 +1,17 @@
+﻿using BarcodeReader.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace BarcodeReader.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class BarcodeReaderView : ContentPage
+	{
+		public BarcodeReaderView ()
+		{
+            BindingContext = new BarcodeReaderViewModel(Navigation);
+
+			InitializeComponent ();
+		}
+    }
+}
